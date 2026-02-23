c5dc8613c9c70c182b9f71030f6e2e45d306b668
mavyaiman2-tech <mavyaiman2@gmail.com>
2026-02-24 00:29:17 +0200

    Delete src directory

--- src/db/user.reposcitory.js
+++
@@ -1,7 +0,0 @@
-import { User } from "./models/user.model.js";
-import { AbstractRepository } from "./abstract.repository.js";
-export class UserRepository extends AbstractRepository {
-  constructor() {
-    super(User); 
-  }
-}
